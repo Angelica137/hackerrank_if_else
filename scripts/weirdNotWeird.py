f0949d69f2e90ffdf0fc07a0bace9eb7a6e1b9af
Angelica <Angelica137@users.noreply.github.com>
2020-07-18 22:44:32 +0100

    add no. is even and in range 2 to 5

--- scripts/weirdNotWeird.py
+++ scripts/weirdNotWeird.py
@@ -1,3 +1,5 @@
 def weirdNotWeird(n):
     if n % 2 != 0:
         return 'Weird'
+    elif n % 2 == 0 and 2 <= n <= 5:
+        return 'Not weird'
--- tests/test_weirdNotWeird.py
+++ tests/test_weirdNotWeird.py
@@ -4,3 +4,8 @@ from scripts.weirdNotWeird import weirdNotWeird
 def test_odd_no_prints_Weid():
     n = 3
     assert weirdNotWeird(n) == 'Weird'
+
+
+def test_even_no_and_in_range_2_to_5_print_Not_weird():
+    n = 2
+    assert weirdNotWeird(n) == 'Not weird'
